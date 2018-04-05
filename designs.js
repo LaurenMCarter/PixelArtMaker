d366003a285476385e40b1b3afae7328b39c9d11
LaurenMCarter <lmhorsey@gmail.com>
2018-04-05 13:02:51 -0400

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,20 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+    var height, width, grid, gridrow;
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+    grid = $('#pixelCanvas');
+    gridrow = $('#pixelCanvas tr');
+
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < height; y++)
+            grid.append('tr');
+    }
+        gridrow.append('td');
+}
